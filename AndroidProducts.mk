08ef06816958a7d02f09e249a13ca4eebdfd014c
“Alex009988” <boeingx37@gmail.com>
2026-09-14 17:14:42 +0300

    LOS 23.2 init

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_karin.mk
+    $(LOCAL_DIR)/lineage_bp4a-karin.mk
--- lineage_karin.mk
+++ lineage_karin.mk
@@ -14,7 +14,9 @@
 # limitations under the License.
 #
 
-
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from karin-common device
 $(call inherit-product, device/sony/karin/device.mk)
 
